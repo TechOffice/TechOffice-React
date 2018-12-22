d42915da534fe7189088c202e9c13d7f5bbda769
ben <ben@techoffice.com>
2018-12-23 00:44:11 +0800

    add persistent drawer example

--- 900-Others/050-UiMaterial/020-UiMaterialReactRouterExample/src/component/DrawerComponent.tsx
+++ 900-Others/050-UiMaterial/020-UiMaterialReactRouterExample/src/component/DrawerComponent.tsx
@@ -4,6 +4,7 @@ import PermanentDrawerComponent from "./drawer/PermanentDrawerComponent";
 import { Link } from "react-router-dom";
 import ResponsiveDrawerComponent from "./drawer/ResponsiveDrawerComponent";
 import TemporaryDrawerComponent from "./drawer/TemporaryDrawerComponent";
+import PersistentDrawerComponent from "./drawer/PersistentDrawerComponent";
 
 
 export default class DrawerComponent extends React.Component<any, any>{
@@ -16,11 +17,12 @@ export default class DrawerComponent extends React.Component<any, any>{
                     <li><Link to="/drawer/permanent">Permanent</Link></li>
                     <li><Link to="/drawer/responsive">Responsive</Link></li>
                     <li><Link to="/drawer/temporary">Temporary</Link></li>
+                    <li><Link to="/drawer/persistent">Persistent</Link></li>
                 </ul>
                 <Route path="/drawer/permanent" component={PermanentDrawerComponent}/>
                 <Route path="/drawer/responsive" component={ResponsiveDrawerComponent}/>
                 <Route path="/drawer/temporary" component={TemporaryDrawerComponent}/>
-
+                <Route path="/drawer/persistent" component={PersistentDrawerComponent}/>
             </div>
         );
     }
