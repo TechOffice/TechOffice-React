595d90ab2a91ead945bab5072bc7eda235a6e6ea
Pan Wai CHENG <panwaicheng@dbs.com>
2019-01-16 15:10:32 +0800

    update

--- 900-Library/010-Main/060-ReduxForm/040-ReduxFormMaterialUiDetailExamples/030-SyncValidation/src/simpleFormValidation.tsx
+++
@@ -1,8 +0,0 @@
-
-export default (values: any) => {
-    const errors: any = {};
-    if (!values.requiredTextField1){
-        errors.requiredTextField1 = 'Required'
-    }
-    return errors;
-};
--- 900-Library/010-Main/060-ReduxForm/040-ReduxFormMaterialUiDetailExamples/040-FieldLevelValidation/src/required.tsx
+++
@@ -1,2 +0,0 @@
-
-export default (value: any) => (value || typeof value === 'number' ? undefined : 'Required');
