e0c3c79c71cd73ce26a4efffe92287448c5a0896
ben <ben@techoffice.com>
2019-03-21 01:23:30 +0800

    update

--- 900-Library/010-Main/999-Dev/010-POS/080-CheckoutWithServerside/src/App.tsx
+++ 900-Library/010-Main/999-Dev/010-POS/080-CheckoutWithServerside/src/App.tsx
@@ -9,6 +9,8 @@ import { MuiPickersUtilsProvider } from 'material-ui-pickers';
 import MomentUtils from '@date-io/moment';
 import store from "./store";
 import CheckoutComponent from "./component/checkout/CheckoutComponent";
+import LoginComponent from "./component/login/LoginComponent";
+import { MemoryRouter } from "react-router";
 
 @(connect( 
     function(state: any){
@@ -22,9 +24,17 @@ class App extends React.Component<any, any>{
     
     constructor(props){
         super(props);
+        this.state = {
+            logined: false
+        }
     }
 
     render(){
+        if (!this.state.logined){
+            return (
+                <LoginComponent login={()=>{this.setState({logined: true})}}/>
+            );
+        }
         return (
             <div style={{ display: 'flex'}}>
                 <AppBar position="fixed" style={{height: 50}}>
@@ -35,8 +45,8 @@ class App extends React.Component<any, any>{
                     <div style={{height: 70}}></div>
                     <CheckoutComponent/>
                 </div>
-            </div>            
-        )//
+            </div>                    
+        );
     }
 }
 
@@ -44,7 +54,9 @@ class App extends React.Component<any, any>{
 ReactDOM.render(
     <Provider store={store}>
         <MuiPickersUtilsProvider utils={MomentUtils}>
-            <App />
+            <MemoryRouter>
+                <App />
+            </MemoryRouter>
         </MuiPickersUtilsProvider>
     </Provider>, 
 document.getElementById('app'));
